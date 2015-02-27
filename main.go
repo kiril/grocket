c28ac010cf97aa925ccd23dabaf05d777eed54eb
Kiril Savino <kiril@gamechanger.io>
2015-02-27 10:33:24 -0500

    omg I'm writing code

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+import "net/http"
+
+var version = "0.0.1"
+
 func main() {
-     print("grocket\n");
+	fmt.Printf("Grocket Server version %s\n", version);
+	fmt.Printf("starting up...")
+	http.HandleFunc("/", basicInfo)
+	http.ListenAndServe(":8080", nil)
+	fmt.Printf("shutting down...")
+}
+
+func basicInfo(writer http.ResponseWriter, r *http.Request) {
+	writer.Write([]byte(fmt.Sprintf("Grocket Server version %s", version)))
 }
