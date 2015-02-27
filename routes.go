dd9adf057346ca6d9e1f4c465f77f742776667a1
Kiril Savino <kiril@gamechanger.io>
2015-02-27 12:23:59 -0500

    next step in tutorial done

--- routes.go
+++ routes.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 
@@ -15,25 +13,6 @@ type Route struct {
 }
 
 type Routes []Route
-
-func NewRouter() *mux.Router {
-	router := mux.NewRouter().StrictSlash(true)
-
-	for _, route := range routes {
-		var handler http.Handler
-
-		handler = route.HandlerFunc
-		handler = Logger(handler, route.Name)
-
-		router.
-			Methods(route.Method).
-			Path(route.Pattern).
-			Name(route.Name).
-			Handler(handler)
-	}
-
-	return router
-}
 
 var routes = Routes{
 	Route{"Status",      "GET",  "/",                              StatusInfo,},
