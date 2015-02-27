87ef38924d322e1ceeecf8d45ba7cd6bc04afe92
Kiril Savino <kiril@gamechanger.io>
2015-02-27 13:26:05 -0500

    an example JSON thing working

--- handlers.go
+++ handlers.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+    "time"
 	"net/http"
-	"encoding/json"
+    "encoding/json"
+
+    "github.com/gorilla/mux"
 )
 
 
@@ -15,6 +18,25 @@ func ScheduleEvent(writer http.ResponseWriter, request *http.Request) {
 }
 
 func ViewEvent(writer http.ResponseWriter, request *http.Request) {
+    vars := mux.Vars(request)
+    eventId := vars["eventId"]
+
+    event := Event{
+        Id: eventId,
+        Time: time.Now(),
+        Payload: "somedata",
+        Expiry: time.Now().Add(time.Minute * 1),
+        EndPoint: "http://dev/null",
+        MaxAttempts: 0,
+        Verb: "PUT",
+    }
+
+    writer.Header().Set("Content-Type", "application/json;charset=UTF-8")
+    writer.WriteHeader(http.StatusOK)
+
+    if error := json.NewEncoder(writer).Encode(event); error != nil {
+        panic(error)
+    }
 }
 
 func EventDue(writer http.ResponseWriter, request *http.Request) {
