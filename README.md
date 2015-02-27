fe0f36ced993258b4275db64259ca5dfbb887fdb
Kiril Savino <kiril@gamechanger.io>
2015-02-27 11:51:00 -0500

    some more thinking/sketching on the API

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # grocket
+
 A dynamic task scheduler service
+
+Grocket is based on the abstraction of an Event.
+
+And Event is characterized by:
+
+* an endpoint that the event is pushed to (a REST API call)
+* a time at which the event should fire
+* a payload, which is a string (could be JSON, could be base64-encoded binary, etc.)
+* a REST verb to use when calling the endpoint (defaults to PUT)
+* a timeout after which the event should be discarded (optionally, defaults to infinite)
+* a max-attempts number (optional, defaults to infinite)
+
+    PUT /events {time: <long/timestamp>,
+                 payload: <string>,
+                 expiry: <long/timestamp>,
+                 endpoint: "https://service.domain.tld/collection",
+                 max-attempts: 1,
+                 verb: <PUT|POST|DELETE|GET>}
--- main.go
+++ main.go
@@ -1,18 +1,50 @@
 package main
 
-import "fmt"
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
 
 var version = "0.0.1"
 
 func main() {
 	fmt.Printf("Grocket Server version %s\n", version);
 	fmt.Printf("starting up...")
-	http.HandleFunc("/", basicInfo)
-	http.ListenAndServe(":8080", nil)
-	fmt.Printf("shutting down...")
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", StatusInfo)
+	router.HandleFunc("/events", ScheduleEvent)
+	router.HandleFunc("/events/{eventId}", ViewEvent)
+	router.HandleFunc("/events/{eventId}/time", ViewEventTime)
+	router.HandleFunc("/events/{eventId}/expiry", ViewEventExpiry)
+	router.HandleFunc("/events/{eventId}/payload", ViewEventPayload)
+	router.HandleFunc("/events/{eventId}/endpoint", ViewEventEndpoint)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func basicInfo(writer http.ResponseWriter, r *http.Request) {
-	writer.Write([]byte(fmt.Sprintf("Grocket Server version %s", version)))
+func StatusInfo(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Grocket Server version %s", version)
+}
+
+func ScheduleEvent(writer http.ResponseWriter, request *http.Request) {
+}
+
+func ViewEvent(writer http.ResponseWriter, request *http.Request) {
+}
+
+func ViewEventTime(writer http.ResponseWriter, request *http.Request) {
+}
+
+func ViewEventExpiry(writer http.ResponseWriter, request *http.Request) {
+}
+
+func ViewEventPayload(writer http.ResponseWriter, request *http.Request) {
+}
+
+
+func ViewEventEndpoint(writer http.ResponseWriter, request *http.Request) {
 }
