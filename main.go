b22c52f138995056c6ba57cf1ee547acb6bd149a
Kiril Savino <kiril@gamechanger.io>
2015-03-04 09:10:51 -0500

    some command-line arguments

--- main.go
+++ main.go
@@ -1,18 +1,52 @@
 package main
 
 import (
+    "os"
 	"fmt"
 	"log"
+    "strconv"
 	"net/http"
 )
 
 var version = "0.0.2"
 
-func main() {
-	fmt.Printf("Grocket Server version %s\n", version);
-	fmt.Printf("starting up...")
+func StartServer(port int) {
+	fmt.Printf("Grocket Server starting up on port %d...\n", port)
 
 	router := NewRouter()
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), router))
+}
+
+func StartClient(port int) {
+}
+
+func main() {
+	fmt.Printf("Grocket version %s\n", version);
+
+    args := os.Args[1:]
+    command := "server"
+    if len(args) >= 1 {
+        command = args[0]
+    }
+
+    switch command {
+    case "server":
+        port := 8080
+        if len(args) > 1 {
+            parsedPort, error := strconv.Atoi(args[1])
+            if error != nil {
+                log.Fatalf("Invalid server port: needs to be int, not %s", args[1])
+            }
+            port = parsedPort
+        }
+        StartServer(port)
+
+    case "client":
+        port, error := strconv.Atoi(args[1])
+        if error != nil {
+            log.Fatalf("Invalid server port: needs to be int, not %s", args[1])
+        }
+        StartClient(port)
+    }
 }
